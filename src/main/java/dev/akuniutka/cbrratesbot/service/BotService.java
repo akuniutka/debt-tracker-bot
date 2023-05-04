e973f83670073b8c616d4619e3e7553a3ab2f776
Andrei Kuniutka <akuniutka@gmail.com>
2023-05-04 21:34:10 +0300

    added /currentrates bot command processing

--- src/main/java/dev/akuniutka/cbrratesbot/service/BotService.java
+++ src/main/java/dev/akuniutka/cbrratesbot/service/BotService.java
@@ -1,12 +1,17 @@
 package dev.akuniutka.cbrratesbot.service;
 
 
+import dev.akuniutka.cbrratesbot.dto.ValuteCursOnDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 
@@ -22,6 +27,24 @@ public class BotService extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        try {
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(String.valueOf(chatId));
+            if ("/currentrates".equalsIgnoreCase(message.getText())) {
+                for (ValuteCursOnDate valuteCursOnDate : cbrService.getCurrenciesFromCbr()) {
+                    response.setText(StringUtils.defaultIfBlank(response.getText(), "") +
+                            valuteCursOnDate.getName() + " - " + valuteCursOnDate.getCourse() + "\n"
+                    );
+                }
+            }
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @PostConstruct
