0b4a465e6c05024ec69d9aa6b14126379b8590bd
Andrei Kuniutka <akuniutka@gmail.com>
2023-05-08 17:43:45 +0300

    added Swagger 3 and downgraded Spring Boot version for it

--- src/main/java/dev/akuniutka/cbrratesbot/controller/ExchangeRatesController.java
+++ src/main/java/dev/akuniutka/cbrratesbot/controller/ExchangeRatesController.java
@@ -2,6 +2,7 @@ package dev.akuniutka.cbrratesbot.controller;
 
 import dev.akuniutka.cbrratesbot.dto.ExchangeRate;
 import dev.akuniutka.cbrratesbot.service.CbrService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +12,18 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/exchangeRates")
 public class ExchangeRatesController {
     private final CbrService cbrService;
 
-    @GetMapping("/exchangeRates")
+    @GetMapping
+    @Operation(summary = "получить текущие курсы валют")
     public List<ExchangeRate> getExchangeRates() throws DatatypeConfigurationException, IllegalStateException {
         return cbrService.getExchangeRates();
     }
 
-    @GetMapping("/exchangeRates/{currency}")
+    @GetMapping("/{currency}")
+    @Operation(summary = "получить текущий курс заданной валюты")
     public ExchangeRate getExchangeRate(@PathVariable String currency) throws
             DatatypeConfigurationException, IllegalStateException, NoSuchElementException {
         return cbrService.getExchangeRate(currency);
