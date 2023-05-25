dc624f9225543ae7f6295c8df2ebd24ceefaef8f
Andrei Kuniutka <akuniutka@gmail.com>
2023-05-25 22:18:24 +0300

    removed dedicated controllers for incomes and expenses: will be managed via Telegram

--- src/main/java/dev/akuniutka/cbrratesbot/Bot.java
+++ src/main/java/dev/akuniutka/cbrratesbot/Bot.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// TODO: find why Telegram API (Scheduled Service?) delays exit
+// TODO: move from Swagger to SpringDoc
+// TODO: move to Spring Boot 2.7.11
+// TODO: refactor the code according to Clean Code
+
 @EnableScheduling
 @SpringBootApplication
 public class Bot {
