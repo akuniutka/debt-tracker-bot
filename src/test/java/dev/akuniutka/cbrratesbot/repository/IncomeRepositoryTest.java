d6734553e6e14155ef4323dcac048e1f7a09d7eb
Andrei Kuniutka <akuniutka@gmail.com>
2023-06-07 23:18:27 +0300

    refactored test for IncomeRepository

--- src/test/java/dev/akuniutka/cbrratesbot/repository/IncomeRepositoryTest.java
+++ src/test/java/dev/akuniutka/cbrratesbot/repository/IncomeRepositoryTest.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,16 +18,9 @@ class IncomeRepositoryTest {
     private IncomeRepository incomeRepository;
 
     @Test
-    void testRpository() {
-        for (int i = 0; i < 10; i++) {
-            Income income = new Income();
-            income.setChatId(444444444L);
-            income.setAmount(BigDecimal.valueOf(555.55));
-            income.setEntryDate(new Date());
-            incomeRepository.save(income);
-        }
-        final List<Income> incomes = incomeRepository.findAll();
-        assertEquals(23, incomes.size());
+    void testFindAll() {
+        List<Income> incomes = incomeRepository.findAll();
+        assertEquals(13, incomes.size());
     }
 
 }
