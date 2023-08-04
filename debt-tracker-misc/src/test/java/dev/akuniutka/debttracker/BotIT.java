6cdd2751cde02fb8d72d57d9a3c8948df44ae3e6
Andrei Kuniutka <akuniutka@gmail.com>
2023-08-04 23:36:56 +0300

    configured a maven wrapper for integration tests

--- debt-tracker-misc/src/test/java/dev/akuniutka/debttracker/BotIT.java
+++ debt-tracker-misc/src/test/java/dev/akuniutka/debttracker/BotIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 // TODO: implement 100% test coverage of the bot
 
 @SpringBootTest
-class BotTests {
+class BotIT {
 
     @Test
     void whenSpringContextLoadsShouldNoBeExceptions() {
