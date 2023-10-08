edeba1216216770111f5288f5fac4bcf1e40add8
Andrei Kuniutka <akuniutka@gmail.com>
2023-10-08 14:18:41 +0300

    Refactored SimpleChatBot

--- simple-chat-bot/src/main/java/dev/akuniutka/chatbot/simplebot/SimpleChatBot.java
+++ simple-chat-bot/src/main/java/dev/akuniutka/chatbot/simplebot/SimpleChatBot.java
@@ -2,20 +2,19 @@ package dev.akuniutka.chatbot.simplebot;
 
 import dev.akuniutka.chatbot.core.Chat;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class SimpleChatBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Chat chat = new Chat((long) scanner.hashCode(), new HelloState());
-        System.out.println("Send message to bot or 'exit' to quit:");
-        String message = scanner.nextLine();
+        String message = "";
         while (!"exit".equals(message)) {
-            List<String> reply = chat.getReplyToMessage(message);
-            System.out.println("Bot:");
-            for (String line : reply) {
-                System.out.println("\t" + line);
+            if (!message.isEmpty()) {
+                System.out.println("Bot:");
+                chat.getReplyToMessage(message).stream()
+                        .map("\t"::concat)
+                        .forEach(System.out::println);
             }
             System.out.println("Send message to bot or 'exit' to quit:");
             message = scanner.nextLine();
